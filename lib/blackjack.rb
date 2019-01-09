943611c41d87ea07893f6e8b474b09805a79d08b
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-09 02:35:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
   rand(1 .. 11)
 end
 
-def display_card_total (number)
-  puts "Your cards add up to #{number}"
+def display_card_total (total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
@@ -18,8 +18,8 @@ def get_user_input
   gets.chomp
 end
 
-def end_game (number)
-  # code #end_game here
+def end_game (total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
